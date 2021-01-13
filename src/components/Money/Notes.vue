891fe9ed896b14483df3c9b8c1d88361c16ce258
tangyou <maoyou5749@163.com>
2021-01-13 14:53:36 +0800

    模块化：把 Money.vue 分成四个子组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,34 @@
+<template>
+  <label class="notes">
+    <span class="name">备注</span>
+    <input type="text" placeholder="在这里输入备注">
+  </label>
+</template>
+
+<script lang="ts">
+export default {
+name: "Notes"
+}
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  display: flex;
+  background: #f5f5f5;
+  font-size: 14px;
+  padding-left: 16px;
+  align-items: center;
+
+  .name {
+    padding-right: 16px;
+  }
+
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background-color: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
