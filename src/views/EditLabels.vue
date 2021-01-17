6e1be5ab08eae060f1aed424c28f6973a694f9ea
tangyou <maoyou5749@163.com>
2021-01-17 22:59:33 +0800

    可以编辑和删除标签

--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -1,15 +1,17 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
       <div class="form-wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="在这里输入标签名"/>
+        <FormItem :value="tag.name"
+                  @update:value="update"
+                  field-name="标签名" placeholder="在这里输入标签名"/>
       </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -26,16 +28,32 @@ import Button from '@/components/Button.vue';
 })
 export default class EditLabels extends Vue {
   tag?: { id: string; name: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter(t => t.id === id)[0];
     if (tag) {
-      this.tag=tag
+      this.tag = tag;
     } else {
       this.$router.replace('/404');
     }
+  }
+
+  update(name: string) {
+    if(this.tag){
+      tagListModel.update(this.tag.id,name)
+    }
+  }
+  remove() {
+    if (this.tag) {
+      tagListModel.remove(this.tag.id);
+      this.$router.back()
+    }
+  }
+  goBack(){
+    this.$router.back()
   }
 }
 </script>
