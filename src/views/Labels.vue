19a967d7405058c6ca9ae211b5013cf58ae24e7c
tangyou <maoyou5749@163.com>
2021-01-23 15:42:58 +0800

    封装 window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,7 +17,6 @@
 </template>
 
 <script lang="ts">
-import tagListModel from '@/models/tagListModel';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
@@ -31,12 +30,7 @@ export default class Labels extends Vue {
   createTag() {
     const name = window.prompt('请输出标签名');
     if (name) {
-      const message = tagListModel.create(name);
-      if (message === 'duplicated') {
-        window.alert('标签名重复了');
-      } else if (message === 'success') {
-        window.alert('添加成功');
-      }
+      window.createTag(name);
     }
   }
 }
