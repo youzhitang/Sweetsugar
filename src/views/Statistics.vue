cd9b432cb22b6db1fe6aaefd555a1936fc6f3fec
tangyou <maoyou5749@163.com>
2021-02-16 21:23:03 +0800

    自己封装 Chart 组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,7 @@
 <template>
   <Layout>
     <Tabs :data-source="recordTypeList" :value.sync="type" class-prefix="type"/>
+    <Chart :options="x"/>
     <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">{{ beautify(group.title) }}<span>￥{{ group.total }}</span></h3>
@@ -27,9 +28,10 @@ import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
+import Chart from '@/components/Chart.vue';
 
 @Component({
-  components: {Tabs},
+  components: {Tabs,Chart},
 })
 export default class Statistics extends Vue {
   type = '-';
@@ -76,14 +78,38 @@ export default class Statistics extends Vue {
       return '今天';
     } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
       return '昨天';
-    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
-      return '前天';
     }
     if (day.isSame(now, 'year')) {
       return day.format('M月D日');
     } else {
       return day.format('YYYY年M月D日');
     }
+  }
+
+  get x() {
+    return {
+      xAxis: {
+        type: 'category',
+        data: [
+          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+        ],
+        type: 'line'
+      }],
+      tooltip: {show: true}
+    };
   }
 
   tagString(tags: Tag[]) {
@@ -97,6 +123,10 @@ export default class Statistics extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.echarts {
+  max-width: 100%;
+  height: 400px;
+}
 .noResult {
   padding: 16px;
   text-align: center;
